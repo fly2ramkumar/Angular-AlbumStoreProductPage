26fba68cf8f0f224c82d7bee6934bbb42c640c89
fly2ramkumar <56063157+fly2ramkumar@users.noreply.github.com>
2020-05-03 18:44:51 +0530

    module4

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Response } from '@angular/http';
-import { Album } from 'app/album';
+import { Album } from '../album';
 
 
 
